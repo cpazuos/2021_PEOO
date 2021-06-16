d594ad1c0e0613d446d0dded40a0b55de7cae59c
cpazuos <61291192+cpazuos@users.noreply.github.com>
2021-06-16 19:53:48 -0300

    Create Olamundo.cs

---
+++ Aula 01/Olamundo.cs
@@ -0,0 +1,8 @@
+using System; // Biblioteca do .NET
+class MainClass // Classe da Aplicação
+{
+  public static void Main() // Método Principal
+  {
+    Console.WriteLine("Hello World"); // Instrução
+  }
+}
